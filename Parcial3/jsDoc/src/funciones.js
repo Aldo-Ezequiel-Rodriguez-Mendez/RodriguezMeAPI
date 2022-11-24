a1ce01ffa78d5be17ccff9b1a00bad1b882f7a7c
Aldo-Ezequiel-Rodriguez-Mendez <70553666+Aldo-Ezequiel-Rodriguez-Mendez@users.noreply.github.com>
2022-11-23 20:34:39 -0600

    jsdoc

---
+++ Parcial3/jsDoc/src/funciones.js
@@ -0,0 +1,10 @@
+/**
+ * Esta funcion eleva un numero regresa el resultado
+ * @param {number} Numero base de la exponenciancion 
+ * @param {number} Numero exponente de la exponenciacion 
+ * @returns number
+ */
+function potencia(numBase,numExponente){
+    return numBase * numExponente;
+}
+// diagonal + * + * + enter
